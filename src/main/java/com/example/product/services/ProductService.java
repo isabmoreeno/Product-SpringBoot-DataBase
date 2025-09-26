63170fc56a9263153399b361d7a70fcf6ef31931
Your Name <1781432512025@fatecitu.local>
2025-09-26 14:32:17 -0300

    Exception Handler

--- src/main/java/com/example/product/services/ProductService.java
+++ src/main/java/com/example/product/services/ProductService.java
@@ -12,51 +12,48 @@ import com.example.product.dtos.ProductRequest;
 import com.example.product.dtos.ProductResponse;
 import com.example.product.mappers.ProductMapper;
 
-
-
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
-    
+
     @Autowired
     private ProductRepository repository;
 
     public List<ProductResponse> getAllProducts() {
-      return repository.findAll()
+        return repository.findAll()
                 .stream()
                 .map(ProductMapper::toDTO)
                 .toList();
 
     }
 
-       public ProductResponse getProductById(long id) {
+    public ProductResponse getProductById(long id) {
         return repository.findById(id)
                 .map(ProductMapper::toDTO)
                 .orElseThrow(() -> new EntityNotFoundException("Produto não cadastrado"));
     }
 
-    public void deleteProductById(long id){
-		if(repository.existsById(id))
-		repository.deleteById(id);
-		    else
-			    throw new EntityNotFoundException("Produto não existe");
+    public void deleteProductById(long id) {
+        if (repository.existsById(id))
+            repository.deleteById(id);
+        else
+            throw new EntityNotFoundException("Produto não existe");
 
-}
+    }
 
     public ProductResponse saveProduct(ProductRequest request) {
         Product product = ProductMapper.toEntity(request);
         Product savedProduct = repository.save(product);
         return ProductMapper.toDTO(savedProduct);
     }
-  
-   public void updateProduct(ProductRequest request, long id)
-   {
+
+    public void updateProduct(ProductRequest request, long id) {
         Product aux = repository.getReferenceById(id);
         aux.setName(request.name());
         aux.setPrice(request.price());
 
         repository.save(aux);
-   }
+    }
 
 }
